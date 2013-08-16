60ff5c2133a4c4a6c344328255e3ac1d3e72297a
Apprentice <apprentice@devbootcamp.com>
2013-08-16 12:20:25 -0700

    create survey controllers

---
+++ app/controllers/surveys_controller.rb
@@ -0,0 +1,17 @@
+get 'surveys/new' do
+  erb :'surveys/new'
+end
+
+get 'surveys/:id' do
+  @survey = Survey.find_by_id(:id)
+  erb :'surveys/show'
+end
+
+post 'surveys' do
+  @survey = Survey.new(params)
+  if @survey.save
+    redirect "surveys/#{@survey.id}"
+  else
+    erb :'surveys/new'
+  end
+end
