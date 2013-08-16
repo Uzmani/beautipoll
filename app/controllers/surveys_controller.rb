04fcd8ecdad0341965be9534621d5f42c3c280ab
Ishwar Nagar <ishwar.nagar@gmail.com>
2013-08-16 12:43:43 -0700

    make minor change to route, to prepend with a slash

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,13 +1,13 @@
-get 'surveys/new' do
+get '/surveys/new' do
   erb :'surveys/new'
 end
 
-get 'surveys/:id' do
+get '/surveys/:id' do
   @survey = Survey.find_by_id(:id)
   erb :'surveys/show'
 end
 
-post 'surveys' do
+post '/surveys' do
   @survey = Survey.new(params)
   if @survey.save
     redirect "surveys/#{@survey.id}"
