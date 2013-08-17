caf0e4f8adab207e2e672cdcba41ce9830481826
Apprentice <apprentice@devbootcamp.com>
2013-08-16 23:38:00 -0700

    added create surveys and questions

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -34,23 +34,47 @@ get '/surveys/new' do
 end
 
 post '/surveys/new' do
-  Survey.new##
-  @title = 
-  erb :'surveys/new_survey'
+  # File.open('public/uploads/' + params["image"][:filename], "w") do |f|
+  #   f.write(params["image"][:tempfile].read)
+  # end
+  # image_url = "uploads/#{params["image"][:filename]}"
+  @survey = Survey.create({
+    title: params[:title],
+    # image_url: image_url,
+    visibility: (params[:make_private] ? 1 : 0),
+    url: SecureRandom.hex(4)
+    })
+  @questions = Question.where(survey_id: @survey.id)
+  session[:survey_id] = @survey.id
+  erb :'surveys/new_survey', :layout => false
 end
 
 get '/surveys/new_q' do
+  @survey = Survey.find(session[:survey_id]) rescue nil
+  session[:survey_id] = nil
   erb :'surveys/question'
 end
 
 post '/surveys/new_q' do
-  @question = 
-  @answers = 
-  erb :'surveys/new_survey'
+  session[:survey_id] = params[:survey_id]
+  survey = Survey.find(session[:survey_id])
+  @question = Question.create({
+    survey: survey,
+    content: params[:content]
+    })
+  choices = params[:choice].split(".y.y.")
+  choices.each do |choice|
+    Choice.create({
+      question: @question,
+      content: choice
+      })
+  end
+  @choices = Choice.where(question_id: @question.id)
+  erb :'surveys/new_survey', :layout => false
 end
 
-post '/survey/complete' do
-  @alter state of survey
+post '/surveys/complete' do
+  session[:survey_id] = nil
   erb :'surveys/thanks'
 end
 
--- public/js/application.js
+++ public/js/application.js
@@ -1,87 +1,121 @@
 $(document).ready(function () {
 
-  $('#add_answer').on('click', function(e){
-    e.preventDefault();
-    $("<input type='text' class='survey_answer' name='answer[]' placeholder='Enter a possible answer here' size='50'>").appendTo('#new_survey');
-  });
+  // $('#add_answer').on('click', function(e){
+  //   e.preventDefault();
+  //   $("<input type='text' class='survey_answer' name='answer[]' placeholder='Enter a possible answer here' size='50'>").appendTo('#new_survey');
+  // });
 
-  $('#add_question').on('click', function(e){
-    e.preventDefault();
-    $("<hr>").appendTo('#new_survey');
-    $("<div class='new_question'>").appendTo('#new_survey');
-    $("<input type='text' class='survey_question' name='question' placeholder='Enter a question here' size='50'>").appendTo('.new_question:last');
-    $("<input type='text' class='survey_answer' name='answer[]' placeholder='Enter a possible answer here' size='50'>").appendTo('.new_question:last');
-    $("<input type='text' class='survey_answer' name='answer[]' placeholder='Enter a possible answer here' size='50'>").appendTo('.new_question:last');
-  });
+  // $('#add_question').on('click', function(e){
+  //   e.preventDefault();
+  //   $("<hr>").appendTo('#new_survey');
+  //   $("<div class='new_question'>").appendTo('#new_survey');
+  //   $("<input type='text' class='survey_question' name='question' placeholder='Enter a question here' size='50'>").appendTo('.new_question:last');
+  //   $("<input type='text' class='survey_answer' name='answer[]' placeholder='Enter a possible answer here' size='50'>").appendTo('.new_question:last');
+  //   $("<input type='text' class='survey_answer' name='answer[]' placeholder='Enter a possible answer here' size='50'>").appendTo('.new_question:last');
+  // });
 
-  $( "#dialog-form" ).dialog({
-    autoOpen: false,
-    height: 300,
-    width: 350,
+  $("#title-form").dialog({
+    autoOpen: true,
+    height: 325,
+    width: 750,
     modal: true,
     buttons: {
-      "Create an account": function() {
-        var bValid = true;
-        allFields.removeClass( "ui-state-error" );
+      "Create your Survey!": function() {
+     
+      var title = $("#title"),
+      // image = $("#survey_image"),
+      make_private = $("#make_private");
 
-        bValid = bValid && checkLength( name, "username", 3, 16 );
-        bValid = bValid && checkLength( email, "email", 6, 80 );
-        bValid = bValid && checkLength( password, "password", 5, 16 );
+      // console.log(image);
 
-        bValid = bValid && checkRegexp( name, /^[a-z]([0-9a-z_])+$/i, "Username may consist of a-z, 0-9, underscores, begin with a letter." );
-        // From jquery.validate.js (by joern), contributed by Scott Gonzalez: http://projects.scottsplayground.com/email_address_validation/
-        bValid = bValid && checkRegexp( email, /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i, "eg. ui@jquery.com" );
-        bValid = bValid && checkRegexp( password, /^([0-9a-zA-Z])+$/, "Password field only allow : a-z 0-9" );
+      var data = "title="+title.val()+"&make_private="+make_private.val();
+      // +"&image="+image.val();
+      $.post('/surveys/new', data, function(response) {
+        $('.container').html(response);
+      });
+      $(this).dialog("close");
 
-        if ( bValid ) {
-          $( "#users tbody" ).append( "<tr>" +
-            "<td>" + name.val() + "</td>" +
-            "<td>" + email.val() + "</td>" +
-            "<td>" + password.val() + "</td>" +
-          "</tr>" );
-          $( this ).dialog( "close" );
-        }
       },
       Cancel: function() {
-        $( this ).dialog( "close" );
+        $( this ).dialog("close");
       }
     },
-    close: function() {
-      allFields.val( "" ).removeClass( "ui-state-error" );
-    }
+    close: function() {}
   });
 
-  $( "#create-user" )
-    .button()
-    .click(function() {
-      $( "#dialog-form" ).dialog( "open" );
-    });
+  $("#question-form").dialog({
+    autoOpen: true,
+    height: 575,
+    width: 750,
+    modal: true,
+    buttons: {
+      "Add your question!": function() {
+     
+      var content = $("#question_content"),
+      choice = $("input[name='choice']").map(function(){
+        return $(this).val();
+      }).get().join(".y.y."),
+      survey_id = $("#survey_id");
 
+      var data = "content="+content.val()+"&survey_id="+survey_id+"&choice="+choice;
+      $.post('/surveys/new_q', data, function(response) {
+        $('.container').html(response);
+      });
+      $(this).dialog("close");
 
-  $('#create_survey').on('submit', function(e){
-    e.preventDefault();
-    var data = $(this).closest('form').serialize();
-    $.post('/surveys/new', data, function(reply){
-      console.log(reply);
-      $('.container').append(reply);
-    });
+      },
+      Cancel: function() {
+        $(this).dialog("close");
+      }
+    },
+    close: function() {}
   });
 
-  $('#create_question').on('submit', function(e){
-    e.preventDefault();
-    var data = $(this).closest('form').serialize();
-    $.post('/questions/new', data, function(reply){
-      $('.container').html(reply);
-    })
+
+  // $(document).on("click", "#new_question", function(e) {
+
+    // $.get('/surveys/new_q', function(response) {
+    //   $('.container').append(response);
+    // })
+
+
+    // $("#question-form").dialog("open");
+  //   return false;
+  // });
+
+  $(document).on("click", "#add_choice", function() {
+    $("#making_questions").append("<input type='text' name='choice' placeholder='Add an answer choice!'>");
+    return false;
   });
 
-  $('#create_answers').on('submit', function(e){
-    e.preventDefault();
-    var data = $(this).closest('form').serialize();
-    $.post('/answers/new', data, function(reply){
-      $('.container').html(reply);
-    })
+  $(document).on("click", "#complete_survey", function() {
+    $("#thanks").dialog();
   });
+
+  // $('#create_survey').on('submit', function(e){
+  //   e.preventDefault();
+  //   var data = $(this).closest('form').serialize();
+  //   $.post('/surveys/new', data, function(reply){
+  //     console.log(reply);
+  //     $('.container').append(reply);
+  //   });
+  // });
+
+  // $('#create_question').on('submit', function(e){
+  //   e.preventDefault();
+  //   var data = $(this).closest('form').serialize();
+  //   $.post('/questions/new', data, function(reply){
+  //     $('.container').html(reply);
+  //   })
+  // });
+
+  // $('#create_answers').on('submit', function(e){
+  //   e.preventDefault();
+  //   var data = $(this).closest('form').serialize();
+  //   $.post('/answers/new', data, function(reply){
+  //     $('.container').html(reply);
+  //   })
+  // });
 
 
 	setTimeout(function() {
